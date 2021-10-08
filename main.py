b5a8572a515b7f4c4e9ccf492d7a38ac49313a1a
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2021-10-08 15:31:21 +0200

    Add example code with random walk and QTimer to Update matplot in 3D

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
 import sys
 from PyQt5.QtWidgets import QApplication, QDialog
+from PyQt5.QtCore import QTimer
 import gui
+import numpy as np
 
 
 class SilGui(QDialog):
@@ -9,12 +11,48 @@ class SilGui(QDialog):
         super(SilGui, self).__init__(parent)
         self.ui = gui.Ui_Dialog()
         self.ui.setupUi(self)
-        self.plot()
+
+        self.initialize_plot()
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.plot)
+
+        self.counter = 0
+        self.timer.start(100)
+
+    def initialize_plot(self):
+        self.data = [self.gen_rand_line(100, 3) for index in range(5)]
+        self.lines = [self.ui.graphWidget.canvas.ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in self.data]
+        self.ui.graphWidget.canvas.ax.set_xlim3d([0.0, 10.0])
+        self.ui.graphWidget.canvas.ax.set_xlabel('X')
+        self.ui.graphWidget.canvas.ax.set_ylim3d([0.0, 10.0])
+        self.ui.graphWidget.canvas.ax.set_ylabel('Y')
+        self.ui.graphWidget.canvas.ax.set_zlim3d([0.0, 10.0])
+        self.ui.graphWidget.canvas.ax.set_zlabel('Z')
 
     def plot(self):
-        x = range(0, 10)
-        y = range(0, 20, 2)
-        self.ui.graphWidget.canvas.ax.plot(x, y)
+        self.lines = self.update_lines(self.counter, self.data, self.lines)
+        _ = [self.ui.graphWidget.canvas.ax.plot(dat[0, 0:1], dat[1, 0:1], dat[2, 0:1])[0] for dat in self.data]
+        self.ui.graphWidget.canvas.flush_events()
+        self.ui.graphWidget.canvas.draw()
+        self.counter += 1
+        print(self.counter)
+        if self.counter > 100:
+            self.timer.stop()
+
+    def gen_rand_line(self, length, dims=2):
+        line_data = np.empty((dims, length))
+        line_data[:, 0] = np.random.rand(dims)
+        for index in range(1, length):
+            step = (np.random.rand(dims) - 0.5) * 1
+            line_data[:, index] = line_data[:, index - 1] + step
+        return line_data
+
+    def update_lines(self, num, data_lines, lines):
+        for line, data in zip(lines, data_lines):
+            line.set_data(data[0:2, :num])
+            line.set_3d_properties(data[2, :num])
+        return lines
 
 
 def main():
